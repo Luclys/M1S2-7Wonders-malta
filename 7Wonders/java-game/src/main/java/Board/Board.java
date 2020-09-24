67a85653954e037a14394a5f7ce8f0ac1ebd1020
mbbou <mb.boufalah@gmail.com>
2020-09-24 15:54:39 +0200

    Creation of Player and Board classes. First "playable" game.

---
+++ 7Wonders/java-game/src/main/java/Board/Board.java
@@ -0,0 +1,52 @@
+package Board;
+
+import gameelements.Card;
+
+import java.util.ArrayList;
+
+
+public class Board {
+    private final ArrayList<Player> players;
+
+
+    public static void main(String[] args) {
+        System.out.println("Starting 7 wonders");
+        Board board = new Board();
+        board.play();
+        board.scores();
+    }
+
+    public Board() {
+        players = new ArrayList<>(1);
+        players.add(new Player(5, 3));
+        // Soustract nb player * 5 to the total coin nb
+    }
+
+    public void play() {
+        players.forEach(player -> player.setCard(initiateCards()));
+        for (int i = 0; i < 7; i++) {
+            for (Player j : players) {
+                j.playCard();
+            }
+        }
+    }
+
+    public void scores() {
+        for (Player j : players) {
+            System.out.println(j);
+        }
+    }
+
+    public ArrayList<Card> initiateCards() {
+        ArrayList<Card> res = new ArrayList<>();
+        res.add(new Card());
+        res.add(new Card());
+        res.add(new Card());
+        res.add(new Card());
+        res.add(new Card());
+        res.add(new Card());
+        res.add(new Card());
+        return res;
+    }
+
+}
---
+++ 7Wonders/java-game/src/main/java/Board/Player.java
@@ -0,0 +1,32 @@
+package Board;
+
+import gameelements.Card;
+
+import java.util.ArrayList;
+
+public class Player {
+    private final int playerId;
+    private int nbCoin;
+    private ArrayList<Card> cardArrayList;
+    private int score;
+
+    public Player(int id, int nbCoin) {
+        playerId = id;
+        this.nbCoin = nbCoin;
+        cardArrayList = new ArrayList<>(7);
+    }
+
+    public void playCard() {
+        System.out.println("I play my card");
+        score += cardArrayList.get(0).getPoint();
+        cardArrayList.remove(0);
+    }
+
+    public String toString() {
+        return "id player :" + playerId + " score :" + score;
+    }
+
+    public void setCard(ArrayList<Card> initiateCards) {
+        this.cardArrayList = initiateCards;
+    }
+}
