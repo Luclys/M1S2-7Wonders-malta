a5a2f404a7e6e23f2d680681f32939dfe3a5c514
mbbou <mb.boufalah@gmail.com>
2020-09-24 16:03:02 +0200

    Adding Common.

---
+++ 7Wonders/java-common/src/main/java/gameelements/Card.java
@@ -0,0 +1,17 @@
+package gameelements;
+
+public class Card {
+    private final int idCard;
+    private final int pointCard;
+
+    public Card() {
+        idCard = 1;
+        pointCard = 1;
+    }
+
+    public int getPoint() {
+        return pointCard;
+    }
+
+
+}
