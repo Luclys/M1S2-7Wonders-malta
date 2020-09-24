298e9b3a83136a1f1644b71f9236ef427df111b6
mbbou <mb.boufalah@gmail.com>
2020-09-24 16:30:59 +0200

    Creating empty tests classes.

---
+++ 7Wonders/java-game/src/test/java/board/BoardTest.java
@@ -0,0 +1,22 @@
+package board;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BoardTest {
+    @Test
+    public void playTest() {
+
+    }
+
+    @Test
+    public void scoresTest() {
+
+    }
+
+    @Test
+    public void initiateCardsTest() {
+
+    }
+}
+
+
---
+++ 7Wonders/java-game/src/test/java/board/PlayerTest.java
@@ -0,0 +1,17 @@
+package board;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class PlayerTest {
+    @Test
+    public void playCardTest() {
+
+    }
+
+    @Test
+    public void setCardTest() {
+
+    }
+}
